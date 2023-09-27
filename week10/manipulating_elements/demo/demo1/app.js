66e414d495f7326817c9b6200af07215d7f48585
Julian <jallende@bu.edu>
2023-09-26 20:23:23 -0700

    saving changes

---
+++ week10/manipulating_elements/demo/demo1/app.js
@@ -0,0 +1,10 @@
+let h_tag = document.querySelector('#head')
+console.log(h_tag)
+
+let inner = h_tag.innerHTML;
+
+let outer = h_tag.outerHTML;
+
+console.log("This is inner HTML: ", inner)
+
+console.log("This is outer HTML: ", inner)
