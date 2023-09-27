c8ba388a714c02152e90d2506438f4f67dabf5e0
Julian <jallende@bu.edu>
2023-09-26 19:32:26 -0700

    Adding kc slide 9 10-C Elements Manipulation

---
+++ week10/manipulating_elements/kcs/slide9/app.js
@@ -0,0 +1,36 @@
+// console.log("Hello from Javascript")
+// console.log("DOCUMENT:", document)
+
+let div_three = document.getElementById('div-three');
+let title_two = document.getElementById('head')
+console.log( "CHECK THIS",title_two)
+// console.log("div_three: ", div_three)
+div_three['style']['border'] = "2px solid blue";
+div_three['style']['fontSize'] = "45px";
+div_three.style.backgroundColor = "green";
+// console.log("Style in Div", div_three['style'])
+
+
+let all_divs = document.getElementsByClassName('container');
+
+// Step 1 change the text
+// This works it changes the text
+// all_divs[1].innerHTML = 'hacked'
+// all_divs[4].innerHTML = 'hacked'
+
+// Step build a simple loop from 0 -> 5
+
+// let counter = 0;
+// while (counter < 5) {
+//     console.log(counter)
+//     counter = counter + 1;
+// }
+
+
+// Combine everything
+let counter = 0;
+while (counter < all_divs.length) {
+    console.log(counter)
+    all_divs[counter].innerHTML = 'hacked'
+    counter = counter + 1;
+}
