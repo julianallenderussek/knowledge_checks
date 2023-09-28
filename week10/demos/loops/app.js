e51db7a9444a12aa730023b0d68d6a77e0bf9b6c
Julian <jallende@bu.edu>
2023-09-27 19:11:52 -0700

    puhsing for and while loop examples

---
+++ week10/demos/loops/app.js
@@ -0,0 +1,23 @@
+let x = 10;
+
+// 1. counter variable
+let counter = 0;
+
+//2. condition
+while (counter < x) {
+    console.log(counter);
+    //3. how the counter will behave
+    counter = counter + 1;
+}
+
+
+// function three things, 1. counter variable, 2. condition, 3. how the counter will behave
+//function (1. counter variable, 2. condition, 3. how the counter will behave)
+
+// for (let i = 0;  i < x; i = i + 1 ) {
+//     console.log(i)
+// }
+
+for (let i = 0;  i < x; i = i + 1 ) {
+    console.log(i)
+}
